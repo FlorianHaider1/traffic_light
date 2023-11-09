1faf062bbb11e0ffd4fa36ab9863d1b250e8e3d5
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-09 14:35:42 +0100

    updated with task and objective

---
+++ traffic_light.py
@@ -0,0 +1,4 @@
+# Traffic Light Sequence Simulator: Objective: Simulate a traffic light system where each light sequence is represented as a tuple (color, duration).
+# Tasks: Create a list of light sequences (e.g., ("green", 60), ("yellow", 10), ("red", 50)). 
+# Use pattern matching to simulate the traffic light operation, printing which light is on and for how long. 
+# Include safety checks to ensure the sequence always goes green -> yellow -> red.
